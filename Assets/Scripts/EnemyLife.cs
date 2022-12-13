6d90c179643a2a9baa9f56eba50c52771f01a4cb
knotek-kyle <70342120+knotek-kyle@users.noreply.github.com>
2022-12-13 07:51:04 -0800

    AI Import

---
+++ Assets/Scripts/EnemyLife.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLife : MonoBehaviour , IDataPersistence
+{
+    [SerializeField] GameObject e;
+    [SerializeField] private string id ;
+    private bool killed = false;
+    
+
+    [ContextMenu("Generate guid for id")]
+
+
+    private void GenerateGuid(){
+        id = System.Guid.NewGuid().ToString();
+    }
+
+    public void LoadData(PlayerData data){
+        data.enemiesKilled.TryGetValue( id , out killed);
+        if(killed){
+            Destroy(e);
+        }
+        
+    }
+
+    public void SaveData(ref PlayerData data){
+        if(data.enemiesKilled.ContainsKey(id)){
+            data.enemiesKilled.Remove(id);
+        }
+        data.enemiesKilled.Add(id, killed);
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.name == "Sphere(Clone)"){
+            Destroy(e);
+            Destroy(collision.gameObject);
+            killed = true;
+        }
+    }
+}
